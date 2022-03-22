3f0db3eeee5e345016f63200848f12d57b4ff98c
Мужецкий Антон <tonich2001@gmail.com>
2022-03-22 21:18:25 +0300

    node server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+	console.log(`Server listening port ${port}`);
+});
